c8573d3250995065120b6f0b4d02e0aead4c5e3f
Eduardo Klein <epklein@gmail.com>
2022-07-16 22:38:02 -0300

    Atualização com dados de julho de 2022

--- Import.R
+++ Import.R
@@ -4,6 +4,7 @@ library(readr)
 data <- read_csv2("http://www.bcb.gov.br/adm/mecir/dadosabertos/MeioCirculante_DadosAbertos.csv",
                   col_names = c("Data", "Familia", "Denom", "Quant"))
 
+data$Data <- as.Date(data$Data, format="%d/%m/%Y")
 data$Denom <- as.numeric(data$Denom)
 
 save(data, file="bcb.RData")
